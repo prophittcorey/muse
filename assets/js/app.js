bf9d55658213c3204d851971371cf9a0f7f19342
Corey Prophitt <git@prophitt.me>
2023-01-20 10:15:21 -0800

    Stubbed some javascript handlers and state

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,32 @@
 (function (w, d){
   'use strict';
 
-  console.log('Locked & Loaded');
+  var player = {
+    state: {
+      track: 0,
+      tracks: d.querySelectorAll('main > ol > li'),
+      mode: 'paused',
+    },
+
+    buttons: {
+      play: d.querySelector('.player > button'),
+    },
+
+    actions: {
+      play: function () {
+        player.state.mode = 'playing';
+      },
+      pause: function () {
+        player.state.mode = 'paused';
+      },
+    },
+  };
+
+  player.buttons.play.addEventListener('click', function () {
+    if (player.state.mode === 'paused') {
+      player.actions.play();
+    } else {
+      player.actions.pause();
+    }
+  });
 })(window, document)
